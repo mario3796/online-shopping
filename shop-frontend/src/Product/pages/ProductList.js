f64f425d348f467447032c5b14a5113924cef478
Mario Haleem <mariohaleem@gmail.com>
2022-11-22 13:39:13 +0200

    edit productList page

--- shop-frontend/src/Product/pages/ProductList.js
+++ shop-frontend/src/Product/pages/ProductList.js
@@ -1,43 +1,62 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useHttpClient } from '../../shared/hooks/http-hook';
+import queryString from 'query-string';
 
 import ProductItem from '../components/ProductItem';
 import Empty from '../../shared/components/Empty/Empty';
+import Pagination from '../../shared/components/Pagination/Pagination';
 import LoadingSpinner from '../../shared/components/LoadingSpinner/LoadingSpinner';
 
 const ProductList = (props) => {
-  const [products, setProducts] = useState([]);
   const { isLoading, sendRequest } = useHttpClient();
+  const { search } = useLocation();
+  const values = queryString.parse(search);
+
+  const [products, setProducts] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    const fetchProducts = async () => {
+    const fetchProducts = async (page = 1) => {
       try {
         const data = await sendRequest(
-          process.env.REACT_APP_BACKEND_URL + 'products'
+          process.env.REACT_APP_BACKEND_URL + 'products?page=' + page
         );
-        console.log(data);
         setProducts(data.products);
+        setTotalItems(data.totalItems);
       } catch (err) {
         console.log(err);
       }
     };
+    +values.page && setPage(+values.page);
 
-    fetchProducts();
-  }, [sendRequest, setProducts]);
+    fetchProducts(page);
+  }, [sendRequest, setProducts, values.page, page]);
 
   let productList = products.map((product) => {
     return <ProductItem key={product._id} product={product} />;
   });
 
-  if (productList.length === 0) {
-    productList = (
-      <Empty>
-        <p>No Product Found!</p>
-      </Empty>
-    );
-  }
-
-  return <div>{isLoading ? <LoadingSpinner /> : productList}</div>;
+  return isLoading ? (
+    <LoadingSpinner />
+  ) : productList.length > 0 ? (
+    <div>
+      {productList}
+      <Pagination
+        currentPage={page}
+        hasNextPage={2 * page < totalItems}
+        hasPreviousPage={page > 1}
+        nextPage={page + 1}
+        previousPage={page - 1}
+        lastPage={totalItems / 2}
+      />
+    </div>
+  ) : (
+    <Empty>
+      <p>No Product Found!</p>
+    </Empty>
+  );
 };
 
 export default ProductList;
